7bd8d22445ae7ebca3ab612d8eb0cea9fa61fdfa
javier <javier.felix@gcabsa.com.mx>
2022-05-02 16:55:31 -0700

    Configuracion para github pages

---
+++ js/about.ec53dad5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue3_pwa"]=self["webpackChunkvue3_pwa"]||[]).push([[443],{7381:function(n,u,a){a.r(u),a.d(u,{default:function(){return l}});var e=a(3396);const t={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,e.wg)(),(0,e.iD)("div",t,s)}var i=a(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.ec53dad5.js.map
